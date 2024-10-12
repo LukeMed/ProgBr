32b0ecef2967ad8a20810ceeadd2b3da2c42d384
LukeMed <luke.lam1988@gmail.com>
2024-10-12 08:44:08 -0300

    Update12/10/2024

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-let nome = "Lucas"
+let nome = "Miguel"
+let DevFront = "Miguel"
+let resultado = (nome == DevFront)
 
-console.log(nome)
+
+console.log("O Miguel é Desenvolvedor Front? " + resultado)
+
+//Raiz quadrada
+const raiz = Math.sqrt(625)
+
+//Raiz Cúbica
+const raizCubica = Math.cbrt(729)
+const resultadoRaiz = Math.pow(2, 4/4)
+
+console.log(raiz, raizCubica, resultadoRaiz)
